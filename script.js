14fa8ba842a07522100e0dbee1c43b50e8541d92
Justina1106 <jbycenkova@gmail.com>
2023-09-19 19:58:39 +0300

    Pasisveikinimas pridėtas

--- script.js
+++ script.js
@@ -123,3 +123,69 @@ if (correct1 === answer1 && correct2 === answer2 && correct3 === answer3) {
 }
 
 console.groupEnd()
+
+console.groupCollapsed('PASISVEIKINIMAS')
+// Pasisveikinimas
+// 1. Jeigu vartotojas prisijungęs (true/false), tai prie pasisveikinimo reikia prirašyti jo vardą, pvz. „Good Morning, Tom.".
+// 2. Jeigu vartotojas nėra prisijungęs, tai išvesti tik tekstą „Good Morning".
+// 3. Priklausomai nuo paros laiko, pasisveikinimas turėtų būti skirtingas:
+// 3.1. 5-12 val. „Good Morning"
+// 3.2. 13-18 val. „Good Afternoon"
+// 3.3. 19-4 val. „Good Evening"
+// 4. Jeigu vartotojas yra ir prisijungęs, ir šiandien yra jo gimtadienis, tai prie pasisveikinimo dar turi būti parašytas ir pasveikinimas, pvz.: „Good Morning, Tom and have a great birthday!"
+
+let isLoggedIn = true
+let userName = 'John'
+let time = 18
+let isBirthday = true
+
+if (isLoggedIn === true) {
+  
+    if (time >= 5 && time < 13) {
+
+        if (isBirthday === true) {
+            console.log(`Good Morning, ${userName} and have a great birthday!`)
+        } else {
+            console.log(`Good Morning, ${userName}.`)
+        }
+
+    } else if (time >= 13 && time < 19) {
+
+        if (isBirthday === true) {
+            console.log(`Good Afternoon, ${userName} and have a great birthday!`)
+        } else {
+            console.log(`Good Afternoon, ${userName}.`)
+        }
+
+    } else if ((time >= 19 && time < 24) || (time >= 0 && time < 5)) {
+
+        if (isBirthday === true) {
+            console.log(`Good Evening, ${userName} and have a great birthday!`)
+        } else {
+            console.log(`Good Evening, ${userName}.`)
+        }
+
+    } else {
+    
+     if (isBirthday === true) {
+        console.log(`Hello, ${userName} and have a great birthday!`)
+     } else {
+        console.log(`Hello, ${userName}.`)
+        }
+
+    }
+
+    } else {
+  
+    if (time >= 5 && time < 13) {
+        console.log(`Good Morning`)
+    }  else if (time >= 13 && time < 19) {
+        console.log(`Good Afternoon`)
+    } else if ((time >= 19 && time < 24) || (time >= 0 && time < 5)) {
+        console.log(`Good Evening`)
+    } else {
+        console.log(`Hello`)
+    }
+}
+
+console.groupEnd()
